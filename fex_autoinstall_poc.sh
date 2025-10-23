9af71627e1252fd703530f21bfdb0fee121c70ec
Mitchell Augustin <mitchell.augustin@canonical.com>
2025-10-23 12:59:50 -0500

    Enable thunking for graphics libraries

--- fex_autoinstall_poc.sh
+++ fex_autoinstall_poc.sh
@@ -7,17 +7,9 @@ wget https://repo.steampowered.com/steam/archive/stable/steam-launcher_latest_al
 wget https://raw.githubusercontent.com/MitchellAugustin/fex_autoinstall/refs/heads/main/patch_steam_for_arm64.patch
 sudo apt install ./steam-launcher_latest_all.deb
 FEXRootFSFetcher -y -a
-cd $HOME/.fex-emu/RootFS
-unsquashfs -f -d NewRootFS/ Ubuntu_24_04.sqsh
-cd NewRootFS
-rm ubuntu_rootfs_update.py
-wget https://raw.githubusercontent.com/MitchellAugustin/fex_autoinstall/refs/heads/main/ubuntu_rootfs_update.py
-chmod +x ubuntu_rootfs_update.py
-./ubuntu_rootfs_update.py chroot
-echo "PWD: " $(pwd)
-cd ../
-mv Ubuntu_24_04.sqsh Ubuntu2404-old-$(date +%s).sqsh
-mksquashfs NewRootFS/ Ubuntu_24_04.sqsh -comp zstd
+
+echo "{"Config":{"Disassemble":"0","PassManagerDumpIR":"0","HostFeatures":"0","ProfileStats":"0","TelemetryDirectory":"","OutputLog":"server","SilentLog":"1","DisableTelemetry":"0","ForceSVEWidth":"0","InjectLibSegFault":"0","GDBSymbols":"0","BlockJITNaming":"0","LibraryJITNaming":"0","GlobalJITNaming":"0","O0":"0","RootFS":"Ubuntu_24_04.sqsh","SMCChecks":"1","X87ReducedPrecision":"0","ThunkConfig":"","X87StrictReducedPrecision":"0","ABILocalFlags":"0","StallProcess":"0","SingleStep":"0","HideHypervisorBit":"0","GdbServer":"0","StartupSleep":"0","DumpIR":"no","StartupSleepProcName":"","MonoHacks":"1","DumpGPRs":"0","ServerSocketPath":"","NeedsSeccomp":"0","ExtendedVolatileMetadata":"","ParanoidTSO":"0","DISABLE_VIXL_INDIRECT_RUNTIME_CALLS":"1","VolatileMetadata":"1","ThunkGuestLibs":"\/usr\/share\/fex-emu\/GuestThunks","StrictInProcessSplitLocks":"0","ThunkHostLibs":"\/usr\/lib\/aarch64-linux-gnu\/fex-emu\/HostThunks","HalfBarrierTSOEnabled":"1","MemcpySetTSOEnabled":"0","SmallTSCScale":"1","VectorTSOEnabled":"0","MaxInst":"5000","TSOEnabled":"1","Multiblock":"1"},"ThunksDB":{"fex_thunk_test":0,"asound":0,"drm":0,"Vulkan":1,"WaylandClient":0,"GL":1}}" > ~/.fex-emu/Config.json
+
 echo "abi <abi/4.0>,
 include <tunables/global>
  
@@ -33,5 +25,4 @@ sudo apparmor_parser -Tr /etc/apparmor.d/steam
 sudo apparmor_parser -Tr /etc/apparmor.d/FEXBash
 cd $SWD
 sudo patch -p1 /usr/lib/steam/bin_steam.sh < patch_steam_for_arm64.patch
-echo "We recommend running sudo apt upgrade to ensure everything on your host is up-to-date"
-echo "Run FEXBash steam to start steam"
+echo "We recommend running sudo apt update && sudo apt upgrade to ensure everything on your host is up-to-date"
